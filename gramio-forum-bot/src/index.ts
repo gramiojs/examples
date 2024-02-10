f8f87f586f7f43ec7bd37da9b20558235be07e24
Kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-02-10 14:40:29 +0300

    feat: add release.created trigger and compare link to push event

--- gramio-forum-bot/src/index.ts
+++ gramio-forum-bot/src/index.ts
@@ -2,11 +2,22 @@ import { github } from "./github";
 import { sendToGithubTopic } from "./telegram";
 import "./webhook";
 
-github.webhooks.on("push", ({ payload: { repository, commits } }) => {
+github.webhooks.on("push", ({ payload: { repository, commits, compare } }) => {
 	if (repository.visibility === "public")
 		sendToGithubTopic(
 			`${repository.full_name}\n\n${commits
 				.map((commit) => `- ${commit.message}`)
-				.join("\n")}`,
+				.join("\n")}\n\n${compare}`,
 		);
 });
+
+github.webhooks.on(
+	"release.created",
+	({ payload: { release, repository } }) => {
+		sendToGithubTopic(
+			`🎉 ${repository.full_name}@${release.tag_name.replace("v", "")}\n\n${
+				release.body
+			}`,
+		);
+	},
+);
