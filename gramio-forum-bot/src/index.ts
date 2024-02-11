131f05aa96c780fc163382400e845058c619f918
Kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-02-12 01:13:13 +0300

    chore: use new format strip indent API!

--- gramio-forum-bot/src/index.ts
+++ gramio-forum-bot/src/index.ts
@@ -7,9 +7,11 @@ github.webhooks.on("push", ({ payload: { repository, commits, compare } }) => {
 	if (repository.visibility !== "public" || !commits.length) return;
 
 	sendToGithubTopic(
-		format`${link(repository.full_name, repository.url)}\n\n${commits
-			.map((commit) => `- ${commit.message}`)
-			.join("\n")}\n\n${link("Compare changes", compare)}`,
+		format`${link(repository.full_name, repository.url)}
+		
+		${commits.map((commit) => `- ${commit.message}`).join("\n")}
+			
+		${link("Compare changes", compare)}`,
 	);
 });
 
@@ -20,7 +22,9 @@ github.webhooks.on(
 			format`🎉 ${link(
 				`${repository.full_name}@${release.tag_name.replace("v", "")}`,
 				repository.url,
-			)}\n\n${release.body}`,
+			)}
+			
+			${release.body}`,
 		);
 	},
 );
