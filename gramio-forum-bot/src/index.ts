5da7a1ca573add5b189847f7baf4f86755826ec4
kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-05-16 11:00:12 +0300

    chore: try to ignore renovate pushes

--- gramio-forum-bot/src/index.ts
+++ gramio-forum-bot/src/index.ts
@@ -3,19 +3,27 @@ import { github } from "./github";
 import { sendToGithubTopic } from "./telegram";
 import "./webhook";
 
-github.webhooks.on("push", ({ payload: { repository, commits, compare } }) => {
-	if (repository.visibility !== "public" || !commits.length) return;
+github.webhooks.on(
+	"push",
+	({ payload: { repository, commits, compare, pusher } }) => {
+		if (
+			repository.visibility !== "public" ||
+			!commits.length ||
+			pusher.name === "renovate[bot]"
+		)
+			return;
 
-	sendToGithubTopic(
-		format`${link(repository.full_name, repository.url)}
+		sendToGithubTopic(
+			format`${link(repository.full_name, repository.url)}
 		
 
 		${commits.map((commit) => `- ${commit.message}`).join("\n")}
 
 
 		${link("Compare changes", compare)}`,
-	);
-});
+		);
+	},
+);
 
 github.webhooks.on(
 	"release.created",
